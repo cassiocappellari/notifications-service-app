5dc93bd355ac81cccc55eeda08598217cce18c22
cassiocappellari <cassiocappellari@gmail.com>
2025-04-10 14:33:19 -0300

    feat: create mapper pattern for the prisma layer

--- src/infra/database/prisma/repositories/prisma-notification-repository.ts
+++ src/infra/database/prisma/repositories/prisma-notification-repository.ts
@@ -2,22 +2,18 @@ import { Notification } from 'src/app/entities/notification';
 import { NotificationRepository } from 'src/app/repositories/notification-repository';
 import { PrismaService } from '../prisma.service';
 import { Injectable } from '@nestjs/common';
+import { PrismaNotificationMapper } from '../mappers/prisma-notification-mapper';
 
 @Injectable()
 export class PrismaNotificationRepository implements NotificationRepository {
   constructor(private prismaService: PrismaService) {}
 
   async create(notification: Notification): Promise<void> {
-    const { content, category, recipientId, readAt, id } = notification;
+    const notificationToPrisma =
+      PrismaNotificationMapper.toPrisma(notification);
 
     await this.prismaService.notification.create({
-      data: {
-        id,
-        content: content.value,
-        category,
-        recipientId,
-        readAt,
-      },
+      data: notificationToPrisma,
     });
   }
 }
